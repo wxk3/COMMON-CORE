134c8e65b1a06eb198448a91770c59b48405702f
Gonçalo Ferreira <goncaloferreira@MacBook-Air-de-Goncalo.local>
2023-11-08 20:04:12 +0000

    update

--- printf/ft_printf.c
+++ printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: goncaloferreira <goncaloferreira@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 16:10:29 by gonferna          #+#    #+#             */
-/*   Updated: 2023/11/07 17:54:42 by goncaloferr      ###   ########.fr       */
+/*   Updated: 2023/11/08 19:56:47 by goncaloferr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ int		ft_formats(va_list args, const char format)
 		print_length += ft_printchar(va_arg(args, int));
 	else if (format == 's')
 		print_length += ft_printstr(va_arg(args, char *));
+	else if (format == 'p')
+		print_length += ft_pointer_printer(va_arg(args, unsigned long long));
 	else if (format == 'd' || format == 'i')
 		print_length += ft_print_d_i(va_arg(args, int));
 	else if (format == 'u')
@@ -36,5 +38,29 @@ int		ft_formats(va_list args, const char format)
 		print_length += ft_print_hex(va_arg(args, unsigned int), format);
 	else if (format == '%')
 		print_length += ft_print_percent();
+	return (print_length);
+}
+
+int		ft_printf(const char *str, ...)
+{
+	int		i;
+	va_list	args;
+	int		print_length;
+
+	i = 0;
+	print_length = 0;
+	va_start(args, str);
+	while (str[i])
+	{
+		if (str[i] == '%')
+		{
+			print_length += ft_formats(args, str[i + 1]);
+			i++;
+		}
+		else
+			print_length += ft_formats(args, str[i]);
+		i++;
+	}
+	va_end(args);
 	return (print_length);
 }
--- printf/ft_printf.h
+++ printf/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: goncaloferreira <goncaloferreira@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 17:44:21 by goncaloferr       #+#    #+#             */
-/*   Updated: 2023/11/07 18:23:52 by goncaloferr      ###   ########.fr       */
+/*   Updated: 2023/11/08 20:01:52 by goncaloferr      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,21 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-int		ft_printchar(int c);
+int		ft_printf(const char *str, ...);
 int		ft_formats(va_list args, const char format);
-int	ft_printstr(char *str);
-int		ft_hex_len(unsigned int num);
-void	ft_put_hex(unsigned int	num, const char format);
-int		ft_printf_hex(unsigned int	num, const char format);
-int	ft_print_percent(void);
+int		ft_printchar(int c);
+int     ft_printstr(char *str);
+int     ft_pointer_printer(unsigned long long ptr)
 int		ft_print_d_i(int n);
 int     ft_print_unsigned(unsigned int n);
-char    *ft_uitoa(unsigned int n);
+int		ft_printf_hex(unsigned int	num, const char format);
+int     ft_print_percent(void);
+
+int     ft_ptr_len(uintptr_t num);
+void    ft_put_ptr(uintptr_t num);
 int     ft_numlength(unsigned int num);
+void	ft_put_hex(unsigned int	num, const char format);
+char    *ft_uitoa(unsigned int n);
+int		ft_hex_len(unsigned int num);
 
 #endif
