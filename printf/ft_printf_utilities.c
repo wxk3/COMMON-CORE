09483e5bf45219bd91b5e5b8c4da238996229baa
Goncalo Fernandes Ferreira <gonferna@c2r4s4.42lisboa.com>
2023-11-17 18:31:23 +0000

    updta

--- printf/ft_printf_utilities.c
+++ printf/ft_printf_utilities.c
@@ -6,11 +6,23 @@
 /*   By: gonferna <gonferna@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 17:00:33 by gonferna          #+#    #+#             */
-/*   Updated: 2023/11/12 16:48:48 by gonferna         ###   ########.fr       */
+/*   Updated: 2023/11/17 18:30:13 by gonferna         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
 
 int	ft_printstr(char *str)
 {
@@ -19,9 +31,10 @@ int	ft_printstr(char *str)
 	i = 0;
 	if (str == NULL)
 	{
-		return (0);
+		ft_putstr("(null)");
+		return (6);
 	}
-	while (str[i] != '\0')
+	while (str[i])
 	{
 		write(1, &str[i], 1);
 		i++;
