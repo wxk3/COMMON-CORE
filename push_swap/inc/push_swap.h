c1e0a0bbb9a4387cd05db657bdcca5f5c888b568
wxk3 <goncalo.fernandess.f@gmail.com>
2024-05-20 02:21:38 +0100

    wxke

---
+++ push_swap/inc/push_swap.h
@@ -0,0 +1,32 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../../libft/libft.h"
+# include "../../printf/ft_printf.h"
+# include <limits.h>
+# include <stdbool.h>
+
+typedef struct s_stack_node
+{
+	int					nbr;
+	int					index;
+	int					push_cost;
+	bool				above_median;
+	bool				cheapest;
+	struct s_stack_node	*target_node;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}						t_stack_node;
+
+// Handle errors
+
+// Stack initiation
+
+// Nodes initiation
+
+// Stack utils
+
+// Commands
+
+// Algorithms
+#endif
